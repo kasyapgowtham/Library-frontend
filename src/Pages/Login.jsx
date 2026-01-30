9ba2f0276291d42701fdcc83c15b8d54704eccb5
Gowtham <kasyapgowtham@gmil.com>
2026-01-30 20:15:16 +0530

    updating login page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -14,11 +14,6 @@ export default function Login() {
                 studentId:studentId,
                 StudentPassword:password,
             },
-                  {
-        headers: {
-          "Content-Type": "application/json"
-        }
-      }
         );
             console.log('Login successful:', response.data);
             navigate('/home');
